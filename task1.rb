425978a203e06c5615ad02ef735f3b59408984f6
Якунин Иван <iv.ya@list.ru>
2025-05-12 16:07:21 +0300

    Первая задача

---
+++ task1.rb
@@ -0,0 +1,7 @@
+puts "Введите имя:"
+name = gets.chomp
+puts "Введите отчество:"
+patronymic = gets.chomp
+puts "Введите фамилию:"
+surname = gets.chomp
+puts "Привет, #{surname} #{name} #{patronymic}"
